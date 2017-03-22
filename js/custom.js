cc6647f10c59be65896e68def8afffe85ca69d2a
Robert Juhasz <robertjuhasz@t-online.hu>
2017-03-22 19:57:31 +0100

    Remove unused code.

--- js/custom.js
+++ js/custom.js
@@ -10,40 +10,10 @@
         event.preventDefault();
     });
 
-    // Highlight the top nav as scrolling occurs
-    /*$('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });*/
-
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
         $('.navbar-toggle:visible').click();
     });
-
-    // Offset for Main Navigation
-    /*$('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })*/
-
-    // Initialize and Configure Scroll Reveal Animation
-    /*window.sr = ScrollReveal();
-    sr.reveal('.sr-icons', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
-    sr.reveal('.sr-button', {
-        duration: 1000,
-        delay: 200
-    });
-    sr.reveal('.sr-contact', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 300);*/
 
     // Initialize and Configure Magnific Popup Lightbox Plugin
     $('.popup-gallery').magnificPopup({
